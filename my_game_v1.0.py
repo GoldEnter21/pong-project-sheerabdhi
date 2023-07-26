08c67695b3015499d7abbd0f570212ecb1e532fa
Sheerabdhi <sheerabdhi@gmail.com>
2023-07-26 13:29:51 -0400

    second commit

---
+++ my_game_v1.0.py
@@ -0,0 +1,91 @@
+import pygame
+import random
+
+# Initialize Pygame
+pygame.init()
+
+# version 1.2!
+
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BALL_SPEED_X = 5
+BALL_SPEED_Y = 5
+PADDLE_SPEED = 8
+PADDLE_WIDTH = 15
+PADDLE_HEIGHT = 100
+BALL_SIZE = 20
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Create the screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Pong")
+
+# Create paddles
+player_paddle = pygame.Rect(50, SCREEN_HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+opponent_paddle = pygame.Rect(SCREEN_WIDTH - 50 - PADDLE_WIDTH, SCREEN_HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+# Create ball
+ball = pygame.Rect(SCREEN_WIDTH // 2 - BALL_SIZE // 2, SCREEN_HEIGHT // 2 - BALL_SIZE // 2, BALL_SIZE, BALL_SIZE)
+
+# Ball movement
+ball_speed_x = BALL_SPEED_X * random.choice((1, -1))
+ball_speed_y = BALL_SPEED_Y * random.choice((1, -1))
+# Draw paddles and ball
+def draw_objects():
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, player_paddle)
+    pygame.draw.rect(screen, WHITE, opponent_paddle)
+    pygame.draw.ellipse(screen, WHITE, ball)
+
+# Move paddles
+def move_paddles():
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and player_paddle.top > 0:
+        player_paddle.y -= PADDLE_SPEED
+    if keys[pygame.K_s] and player_paddle.bottom < SCREEN_HEIGHT:
+        player_paddle.y += PADDLE_SPEED
+    if keys[pygame.K_UP] and opponent_paddle.top > 0:
+        opponent_paddle.y -= PADDLE_SPEED
+    if keys[pygame.K_DOWN] and opponent_paddle.bottom < SCREEN_HEIGHT:
+        opponent_paddle.y += PADDLE_SPEED
+
+# Move ball
+def move_ball():
+    global ball_speed_x, ball_speed_y
+
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    # Collision with top and bottom
+    if ball.top <= 0 or ball.bottom >= SCREEN_HEIGHT:
+        ball_speed_y *= -1
+
+    # Collision with paddles
+    if ball.colliderect(player_paddle) or ball.colliderect(opponent_paddle):
+        ball_speed_x *= -1
+
+    # Reset ball if out of bounds
+    if ball.left <= 0 or ball.right >= SCREEN_WIDTH:
+        ball.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        ball_speed_x *= random.choice((1, -1))
+
+# Main game loop
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    move_paddles()
+    move_ball()
+    draw_objects()
+
+    # Draw the score (not implemented in this code)
+    # Add score text using pygame.font.Font()
+
+    pygame.display.update()
+    clock.tick(60)
